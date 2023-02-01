9c7420a683f68b87923044e0bf377fababa64eed
grigoryanhripsime <108063456+grigoryanhripsime@users.noreply.github.com>
2023-02-01 13:16:47 +0400

    Update 3arrays.cpp

--- 3arrays.cpp
+++ 3arrays.cpp
@@ -38,3 +38,48 @@ int main(){
     return 0;
 }
 
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// class arrays {
+//     protected:
+//         double *x, *y, z[10]; int n;
+//     public:
+//         arrays(int n2, double *x2, double *y2){
+//             n=n2; x=x2; y=y2;
+           
+//         }
+//         ~arrays(){
+//             cout<<"Destructor";
+//         }
+//         void stexcel(){
+//             for (int i = 0; i < n; i++) {
+//                 z[i] = x[i] / y[i];
+//             }
+//         }
+//         void tpel(){
+//             for (int i = 0; i < n; i++)
+//                 cout<<z[i]<<" ";
+//             cout<<endl;
+//         }
+// };
+
+// int main(){
+//     double x1[10],y1[10]; int n1;
+    
+//     do cin>>n1; while (n1<1 ||n1>10);
+//             for (int i = 0; i < n1; i++)
+//                 cin>>x1[i];
+//             for (int i = 0; i < n1; i++)
+//                 cin>>y1[i];
+    
+    
+//     arrays ob(n1,x1,y1);
+//     ob.stexcel();
+//     ob.tpel();
+//     return 0;
+// }
